1d5b2cd87ce2b1fa125cdb2fa7fb029ae792097d
heechangkang <heechang1997@gmail.com>
2019-11-01 19:59:56 +0900

    backup

---
+++ java-basics/src/Basics.java
@@ -0,0 +1,13 @@
+
+public class Basics {
+
+	//static is needed to find other methods
+	public static void main(String[] args) {
+		System.out.println("heechangkang");
+		
+		int testNum = Integer.MIN_VALUE + Integer.MAX_VALUE;
+		
+		System.out.println(testNum);
+	}
+
+}
